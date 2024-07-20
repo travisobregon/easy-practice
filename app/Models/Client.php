c983147b0b1417df013be64a081e393ca8c57a00
Travis Obregon <travismobregon@gmail.com>
2024-07-20 16:19:27 +0100

    Use Saloon to fetch clients

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,14 +2,11 @@
 
 namespace App\Models;
 
+use App\Http\Integrations\EasyPractice\EasyPracticeConnector;
+use App\Http\Integrations\EasyPractice\Requests\GetClientsRequest;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Date;
-use Illuminate\Support\Facades\Http;
 use Sushi\Sushi;
 
 class Client extends Model
@@ -19,25 +16,17 @@ class Client extends Model
 
     public function getRows(): array
     {
-        return Cache::remember('clients', Date::now()->addHour(), function () {
-            return $this->fetchClients();
-        });
-    }
+        $easyPractice = new EasyPracticeConnector;
+        $request = new GetClientsRequest;
 
-    public function fetchClients(): array
-    {
-        $response = Http::withToken(Config::get('services.easy-practice.token'))
-            ->acceptJson()
-            ->asJson()
-            ->get('https://system.easypractice.net/api/v1/clients');
-
-        return Collection::make($response->json('data'))->map(function ($client) {
+        return $easyPractice->send($request)->collect('data')->map(function ($client) {
             return Arr::only($client, [
                 'id',
                 'name',
                 'email',
                 'date_of_birth',
                 'profile_image_url',
+                'status',
                 'created_at',
                 'updated_at',
             ]);
